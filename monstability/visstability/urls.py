8d3290ecd95bd1a34711c7e45e85fc93b3ffed19
Dmitriy Mishanin <dmitriymishanin@gmail.com>
2021-09-30 21:00:43 +0300

    Add tablestead.html

--- monstability/visstability/urls.py
+++ monstability/visstability/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('diagram/', views.diagram, name='diagram'),
     path('model/', views.model, name='model'),
+    path('tablestead/', views.tablestead, name='tablestead'),
     path('about/', views.about, name='about'),
 ]
--- monstability/visstability/views.py
+++ monstability/visstability/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Nodes
 
 def index(request):
     context = {}    #словарь для передачи контента в шаблон HTML
@@ -11,6 +12,11 @@ def diagram(request):
 def model(request):
     context = {}
     return render(request, 'model.html', context)
+
+def tablestead(request):
+    nodes = Nodes.objects.all()
+    context = {'nodes': nodes}
+    return render(request, 'tablestead.html', context)
 
 def about(request):
     context = {}
